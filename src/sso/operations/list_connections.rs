f1e7595e13963d40dd893507b17bc77d32c44b2f
Marshall Bowers <marshall@workos.com>
2022-06-09 17:47:40 -0400

    Nevermind, put it back

--- src/sso/operations/list_connections.rs
+++ src/sso/operations/list_connections.rs
@@ -74,6 +74,7 @@ mod test {
             .build();
 
         let _mock = mock("GET", "/connections")
+            .match_query(Matcher::UrlEncoded("order".to_string(), "desc".to_string()))
             .match_header("Authorization", "Bearer sk_example_123456789")
             .with_status(200)
             .with_body(
