796f8a93ab36b5cb19cee7e8397a20db199c61de
Sheldon Vaughn <svaughn@sheldons-mbp.lan>
2022-06-28 14:38:52 -0600

    Add update_organization operation to organizations module

--- src/organizations/operations.rs
+++ src/organizations/operations.rs
@@ -1,7 +1,9 @@
 mod create_organization;
 mod delete_organization;
 mod get_organization;
+mod update_organization;
 
 pub use create_organization::*;
 pub use delete_organization::*;
 pub use get_organization::*;
+pub use update_organization::*;
