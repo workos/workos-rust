2e3989c20d6e52fbd81326da639a89238fb5206f
Sheldon Vaughn <svaughn@sheldons-mbp.lan>
2022-06-29 08:45:12 -0600

    Add listDirectories operation

--- src/directory_sync/operations.rs
+++ src/directory_sync/operations.rs
@@ -2,8 +2,10 @@ mod delete_directory;
 mod get_directory;
 mod get_directory_group;
 mod get_directory_user;
+mod list_directories;
 
 pub use delete_directory::*;
 pub use get_directory::*;
 pub use get_directory_group::*;
 pub use get_directory_user::*;
+pub use list_directories::*;
