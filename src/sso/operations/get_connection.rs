a435ff0a989e5209651143b917533560459d78c5
Marshall Bowers <marshall@workos.com>
2022-06-30 19:02:45 -0400

    Add `GetConnection` example

--- src/sso/operations/get_connection.rs
+++ src/sso/operations/get_connection.rs
@@ -20,6 +20,24 @@ pub trait GetConnection {
     /// Retrieves a [`Connection`] by its ID.
     ///
     /// [WorkOS Docs: Get a Connection](https://workos.com/docs/reference/sso/connection/get)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use workos::WorkOsResult;
+    /// # use workos::sso::*;
+    /// use workos::{ApiKey, WorkOs};
+    ///
+    /// # async fn run() -> WorkOsResult<(), GetConnectionError> {
+    /// let workos = WorkOs::new(&ApiKey::from("sk_example_123456789"));
+    ///
+    /// let connection = workos
+    ///     .sso()
+    ///     .get_connection(&ConnectionId::from("conn_01E2NPPCT7XQ2MVVYDHWGK1WN4"))
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     async fn get_connection(
         &self,
         id: &ConnectionId,
