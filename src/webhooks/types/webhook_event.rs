48c2c552753767c5c3ccd20a8f79bc551f406f6d
Sheldon Vaughn <sheldonvaughn@icloud.com>
2022-06-30 11:53:17 -0600

    Update src/webhooks/types/webhook_event.rs
    Co-authored-by: Marshall Bowers <marshall@workos.com>

--- src/webhooks/types/webhook_event.rs
+++ src/webhooks/types/webhook_event.rs
@@ -40,7 +40,7 @@ pub enum WebhookEvent {
 
     /// [WorkOS Docs: `dsync.group.user_added` Webhook](https://workos.com/docs/reference/webhooks/directory-group#webhooks-dsync.group.user_added)
     #[serde(rename = "dsync.group.user_added")]
-    DirectoryGroupUserAdded(DirectoryGroupUserAddedWebhook),
+    DirectoryUserAddedToGroup(DirectoryUserAddedToGroupWebhook),
 
     /// [WorkOS Docs: `dsync.group.user_removed` Webhook](https://workos.com/docs/reference/webhooks/directory-group#webhooks-dsync.group.user_removed)
     #[serde(rename = "dsync.group.user_removed")]
